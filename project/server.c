7616e21eb629217f7ed2f88f2ecfd8e4f87febec
Joel <jye5@stuy.edu>
2017-01-09 20:24:02 -0500

    some nonfunctional code added

--- project/server.c
+++ project/server.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 int main() {
   int sd = socket(AF_INET, SOCK_STREAM, 0);
@@ -9,6 +12,26 @@ int main() {
 
   bind(sd, f);
 
+
+
+  //per connection, assign ID. Each player is running a client.
+  sendAll("<Player> has joined the game!\n");
+  /* main gameplay */
+  //everyone has connected, with IDs according to connection order
+  sendAll();
+  //get a set timer function in here
+
+  
   return 0;
 }
 
+//to avoid clutter in the main file, script is stored outside
+char * getScript(int line){
+  int fd = open("script.txt", O_RDONLY);
+  if (fd == -1) printf("Script file corrupted\n");
+  //need to iterate through read lines...
+  char lineRet[64];
+  read(fd, lineRet, 64);
+  close(fd);
+  return lineRet;
+}
