588bb8b4d8228473131e871982f122845ba7bc90
Haley Zeng <hzeng@stuy.edu>
2017-01-23 23:48:50 -0500

    progress is sweet

--- chat_server.c
+++ chat_server.c
@@ -304,7 +304,7 @@ int main() {
       char * shm = (char *) shmat(player.shm_id, 0, 0);
       //      printf("reading shm: [%s]\n", shm);
       if ( strlen(shm) ) { //if shm not empty
-
+	printf("passed strlen(shm)\n");
 	//parse the crap outta it RIGHT HER
 	if (!strlen(msgs[i])){
 	  strcpy(msgs[i], shm);
@@ -315,6 +315,8 @@ int main() {
 	  //	  shm = strcpy(shm, emptyStr);
 	}
       }
+      else
+	printf("strlen(shm) failed\n");
       shmdt(shm);
       semup(player.sem_id);
     }
